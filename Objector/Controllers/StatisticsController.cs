641cd2550cfbd81d6877f70b02fbcb74f50dfdda
Jakub Pilich <jakub.pilich@onexgroup.pl>
2022-01-06 16:23:36 +0100

    add sample StatisticsController

---
+++ Objector/Controllers/StatisticsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Objector.Controllers
+{
+    [Route("api/image")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        public StatisticsController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCurrentDate()
+        {
+            return Ok(DateTime.Now);
+        }
+    }
+}
