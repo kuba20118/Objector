a249165afd57c4b87f2a0ed9ac9f0f530565ef1e
Jakub Pilich <jakub.pilich@onexgroup.pl>
2022-01-06 16:37:20 +0100

    add onnx detection lib

---
+++ Objector.ML/DataModels/TinyYoloPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace Objector.ML.DataModels
+{
+    public class TinyYoloPrediction : IOnnxObjectPrediction
+    {
+        [ColumnName("grid")]
+        public float[] PredictedLabels { get; set; }
+    }
+}
--- Objector/Controllers/StatisticsController.cs
+++ Objector/Controllers/StatisticsController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Objector.Controllers
+namespace Objector.API.Controllers
 {
     [Route("api/image")]
     [ApiController]
